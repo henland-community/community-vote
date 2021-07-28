7b4ad83beeb40cf635193706380baead5b7a5b4c
depatchedmode <depatchedmode@gmail.com>
2021-07-28 10:39:05 -0700

    ~ styles: add type styles to proposals nav

--- poll-taquito/src/components/ProposalsNav.tsx
+++ poll-taquito/src/components/ProposalsNav.tsx
@@ -8,11 +8,11 @@ export const ProposalsNav = ({...props}) => {
       className={`proposalsNav ${props.className}`}
     >
       <div className="proposalsNav-primary">
-        <a href="#hereandnow" className="proposalsNav-item">Proposals<sup className="proposalsNav-itemCount">15</sup></a>
-        <a href="#hereandnow" className="proposalsNav-item">Questions<sup className="proposalsNav-itemCount">34</sup></a>
+        <a href="#hereandnow" className="text-l-bold active proposalsNav-item">Proposals<sup className="proposalsNav-itemCount text-xs">15</sup></a>
+        <a href="#hereandnow" className="text-l-bold proposalsNav-item">Questions<sup className="proposalsNav-itemCount text-xs">34</sup></a>
       </div>
       <div className="proposalsNav-secondary">
-        <a href="#hereandnow" className="proposalsNav-item">Past Votes<sup className="proposalsNav-itemCount">123</sup></a>
+        <a href="#hereandnow" className="text-l-light proposalsNav-item">Past Votes<sup className="proposalsNav-itemCount text-xs">123</sup></a>
       </div>
     </nav>
   );
