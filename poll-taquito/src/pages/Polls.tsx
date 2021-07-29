9a085f3b93b0d78ac1861a7e4d31c863f8e3c8a2
depatchedmode <depatchedmode@gmail.com>
2021-07-28 23:18:01 -0700

    ~ more complete Polls page mockup

--- poll-taquito/src/pages/Polls.tsx
+++ poll-taquito/src/pages/Polls.tsx
@@ -1,5 +1,9 @@
 import '../assets/styles/utility-classes.css';
-import './faq.css';
+import './polls.css';
+
+import { ProposalCard } from '../components/ProposalCard';
+import { Select } from '../components/Select';
+import { Pagination } from '../components/Pagination';
 
 export const Polls = (props: any) => {
   const viewTitles: any = {
@@ -9,13 +13,27 @@ export const Polls = (props: any) => {
     'past': 'Past Votes'
   }
   return (
-    <article>
+    <article className="polls">
       <header className="pageHeader">
         <h1>{ viewTitles[props.view] ?? 'All Votes' }</h1>
       </header>
-      <section className="pageSection">
-        <p>here are the polls</p>
+      <nav className="pageSection polls-filters">
+        <Select />
+        <Select />
+      </nav>
+      <section className="pageSection polls-cards">
+        <ProposalCard />
+        <ProposalCard />
+        <ProposalCard />
+        <ProposalCard />
+        <ProposalCard />
+        <ProposalCard />
+        <ProposalCard />
+        <ProposalCard />
       </section>
+      <footer className="pageSection polls-pagination">
+        <Pagination />
+      </footer>
     </article>
   );
 }
