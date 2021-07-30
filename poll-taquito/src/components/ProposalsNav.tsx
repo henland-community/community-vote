84a7e8897829d54bd907899e96a3694130dfd264
kylegrover <kyle@kylegrover.com>
2021-07-29 18:43:05 -0700

    NavLink

--- poll-taquito/src/components/ProposalsNav.tsx
+++ poll-taquito/src/components/ProposalsNav.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
-import { Link, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import '../assets/styles/design-tokens.css';
 import './proposalsNav.css';
 
 export const ProposalsNav = ({...props}) => {
-  const location = useLocation();
   return (
     <nav
       className={`proposalsNav ${props.className??''}`}
     > 
       <div className="wrap">
         <div className="proposalsNav-primary">
-          <Link to="/proposals" className={`text-l-bold ${location.pathname=='/proposals'?'active':''} proposalsNav-item`}>Proposals<sup className="proposalsNav-itemCount text-xs">15</sup></Link>
-          <Link to="/questions" href="#hereandnow" className={`text-l-bold ${location.pathname=='/questions'?'active':''} proposalsNav-item`}>Questions<sup className="proposalsNav-itemCount text-xs">34</sup></Link>
+          <NavLink to="/proposals" className={`text-l-bold proposalsNav-item`}>Proposals<sup className="proposalsNav-itemCount text-xs">15</sup></NavLink>
+          <NavLink to="/questions" href="#hereandnow" className={`text-l-bold proposalsNav-item`}>Questions<sup className="proposalsNav-itemCount text-xs">34</sup></NavLink>
         </div>
         <div className="proposalsNav-secondary">
-          <Link to="/past-votes" href="#hereandnow" className={`text-l-light ${location.pathname=='/past-votes'?'active':''} proposalsNav-item`}>Past Votes<sup className="proposalsNav-itemCount text-xs">123</sup></Link>
+          <NavLink to="/past-votes" href="#hereandnow" className={`text-l-light proposalsNav-item`}>Past Votes<sup className="proposalsNav-itemCount text-xs">123</sup></NavLink>
         </div>
       </div>
     </nav>
