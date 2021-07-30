0593ff380f1d1e1a0e57156970e02d4e57254b14
kylegrover <kyle@kylegrover.com>
2021-07-29 19:09:47 -0700

    rm proposal counts

--- poll-taquito/src/components/ProposalsNav.tsx
+++ poll-taquito/src/components/ProposalsNav.tsx
@@ -4,17 +4,35 @@ import '../assets/styles/design-tokens.css';
 import './proposalsNav.css';
 
 export const ProposalsNav = ({...props}) => {
+  const [proposalCount, setProposalCount] = React.useState(0);
+  const [questionCount, setQuestionCount] = React.useState(0);
+  const [pastVoteCount, setPastVoteCount] = React.useState(0);
   return (
     <nav
       className={`proposalsNav ${props.className??''}`}
     > 
       <div className="wrap">
         <div className="proposalsNav-primary">
-          <NavLink to="/proposals" className={`text-l-bold proposalsNav-item`}>Proposals<sup className="proposalsNav-itemCount text-xs">15</sup></NavLink>
-          <NavLink to="/questions" href="#hereandnow" className={`text-l-bold proposalsNav-item`}>Questions<sup className="proposalsNav-itemCount text-xs">34</sup></NavLink>
+          <NavLink to="/proposals" className={`text-l-bold proposalsNav-item`}>
+            Proposals
+            { proposalCount ? (
+              <span className="proposalsNav-item-count text-xs">{proposalCount}</span>
+             ) : null } 
+          </NavLink>
+          <NavLink to="/questions" href="#hereandnow" className={`text-l-bold proposalsNav-item`}>
+            Questions
+            { questionCount ? (
+              <span className="proposalsNav-item-count text-xs">{questionCount}</span>
+             ) : null }
+          </NavLink>
         </div>
         <div className="proposalsNav-secondary">
-          <NavLink to="/past-votes" href="#hereandnow" className={`text-l-light proposalsNav-item`}>Past Votes<sup className="proposalsNav-itemCount text-xs">123</sup></NavLink>
+          <NavLink to="/past-votes" href="#hereandnow" className={`text-l-light proposalsNav-item`}>
+            Past Votes
+            { pastVoteCount ? (
+              <span className="proposalsNav-item-count text-xs">{pastVoteCount}</span>
+             ) : null }
+          </NavLink>
         </div>
       </div>
     </nav>
