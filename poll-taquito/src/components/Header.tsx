123eb663d8e44ceb12fd62155ba69671d26d857e
kylegrover <kyle@kylegrover.com>
2021-07-29 18:36:29 -0700

    wrap, Link, useWallet

--- poll-taquito/src/components/Header.tsx
+++ poll-taquito/src/components/Header.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import '../assets/styles/design-tokens.css';
 import './header.css';
+import { useWallet } from "@tz-contrib/react-wallet-provider";
 
 import { ReactComponent as Logo } from '../assets/icons/hen-vote-logo.svg';
 
@@ -12,44 +13,54 @@ interface HeaderProps {
   votes: number;
 }
 
+function shortAddr(address:string){
+  return address.slice(0,4)+"..."+address.slice(address.length - 4,address.length)
+}
+
+
 export const Header = ({
   votes = 0,
   ...props
 }: HeaderProps) => {
+  const { disconnect, activeAccount, connect } = useWallet();
+  const addr = activeAccount?shortAddr(activeAccount.address):'';
   return (
     <header
       className="appHeader"
       {...props}
     >
-      <nav className="appHeader-primaryNav">
-        <section className="appHeader-primaryNav-left">
-          <div className="appHeader-logo">
-            <Logo/>
-          </div>
-          <nav className="appHeader-resources">
-            {/* <Link to="/">Home</Link>
-          <Link to="/faq">FAQ</Link>
-          <Link to="/about">About</Link> */}
-          <a target="_blank" rel="noreferrer" href="https://community.hicetnunc.xyz">Forum ↪</a>
-          </nav>
-        </section>
-        <section className="appHeader-primaryNav-right">
-          <div className="appHeader-votingStatus">
-            <div className="votingStatus">
-              <span className="votingStatus-count">
-                { votes } votes
-              </span>
+      <div className="wrap">
+        <nav className="appHeader-primaryNav">
+          <section className="appHeader-primaryNav-left">
+            <div className="appHeader-logo">
+              <Link to="/"><Logo/></Link>
             </div>
-          </div>
-          <div className="appHeader-walletAddress">
-            tz13...123s
-          </div>
-          <SyncMenu 
-            synced={false}
-            admin={false}
-          />
-        </section>
-      </nav>
+            <nav className="appHeader-resources">
+              <Link to="/">Home</Link>
+              <Link to="/faq">FAQ</Link>
+              <Link to="/about">About</Link>
+              <a target="_blank" rel="noreferrer" href="https://community.hicetnunc.xyz">Forum ↪</a>
+            </nav>
+          </section>
+          <section className="appHeader-primaryNav-right">
+            <div className="appHeader-votingStatus">
+              <div className="votingStatus">
+                <span className="votingStatus-count">
+                  { votes } votes
+                </span>
+              </div>
+            </div>
+            <div className="appHeader-walletAddress">
+              {addr}
+            </div>
+            <SyncMenu 
+              synced={false}
+              admin={false}
+              addr={addr}
+            />
+          </section>
+        </nav>
+      </div>
       <ProposalsNav className="appHeader-proposalsNav" />
     </header>
   );
--- poll-taquito/src/components/SyncMenu.tsx
+++ poll-taquito/src/components/SyncMenu.tsx
@@ -1,59 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from "react-router-dom";
 import '../assets/styles/design-tokens.css';
 import './syncMenu.css';
 import { ProposalsNav } from './ProposalsNav';
 import { ReactComponent as Logo } from '../assets/icons/hen-vote-logo.svg';
 import { ReactComponent as MenuOpen } from '../assets/icons/menu-open.svg';
 import { ReactComponent as MenuClose } from '../assets/icons/menu-close.svg';
-import { useState } from 'react';
+import { useWallet } from "@tz-contrib/react-wallet-provider";
 // import { truncateSync } from 'fs';
 
 interface SyncProps {
   synced: boolean,
-  admin: boolean
+  admin: boolean,
+  addr: string
 }
-
 export const SyncMenu = ({
   synced = false,
   admin = false,
+  addr = '',
   ...props
 }: SyncProps) => {
+  const { disconnect, activeAccount, connect } = useWallet();
   const [open, setOpen] = useState(false);
   return (
     <nav
       className="syncMenu"
       {...props}
     >
-      { open ? 
-          <MenuClose className="syncMenu-close" onClick={(event: any) => { setOpen(false); }} /> 
-        : <MenuOpen className="syncMenu-open" onClick={(event: any) => { setOpen(true) }} />  
+      { open ? <MenuClose className="syncMenu-close" onClick={(event: any) => { setOpen(false); }} /> : <MenuOpen className="syncMenu-open" onClick={(event: any) => { setOpen(true) }} />  
       }
       { open && 
         <div className="syncMenu-contents">
           <section className="syncMenu-mobileOnly">
-            <Logo className="syncMenu-logo"/><br/>
-            <ProposalsNav />
+            <div className="wrap">
+              <Link to="/"><Logo className="syncMenu-logo"/></Link><br/>
+              <MenuClose className="syncMenu-close" onClick={(event: any) => { setOpen(false); }} />
+              <ProposalsNav />
+            </div>
           </section>
           <section className="syncMenu-mainOptions">
-            <section className="syncMenu-section">
-              <a href="#hereandnow" className="syncMenu-item">my votes</a>
-              <a href="#hereandnow" className="syncMenu-item">profile</a>
+            <section className="syncMenu-section wrap">
+              <Link to="/my-votes" className="syncMenu-item" onClick={(event: any) => { setOpen(false); }}>my votes</Link>
+              <Link to="/profile" className="syncMenu-item" onClick={(event: any) => { setOpen(false); }}>profile</Link>
             </section>
             { admin && 
-            <section className="syncMenu-section">
-              <a href="#hereandnow" className="syncMenu-item">create</a>
-              <a href="#hereandnow" className="syncMenu-item">settings</a>
+            <section className="syncMenu-section wrap">
+              <Link to="/admin" className="syncMenu-item" onClick={(event: any) => { setOpen(false); }}>create</Link>
+              <Link to="/profile" className="syncMenu-item" onClick={(event: any) => { setOpen(false); }}>settings</Link>
             </section> }
-            <section className="syncMenu-section mobileOnly">
-              <a href="#hereandnow" className="syncMenu-item">About</a>
-              <a href="#hereandnow" className="syncMenu-item">FAQ</a>
-              <a href="#hereandnow" className="syncMenu-item">Discourse</a>
+            <section className="syncMenu-section mobileOnly wrap">
+              <Link to="/about" className="syncMenu-item" onClick={(event: any) => { setOpen(false); }}>About</Link>
+              <Link to="/faq" className="syncMenu-item" onClick={(event: any) => { setOpen(false); }}>FAQ</Link>
+              <a target="_blank" rel="noreferrer" href="https://community.hicetnunc.xyz" className="syncMenu-item">Discourse</a>
             </section>
-            <section className="syncMenu-section">
-              <a href="#hereandnow" className="syncMenu-item">
-                { synced && <div>tz33...39ab</div> }
-                <div>{ synced ? 'Unsync' : 'Sync'}</div>
-              </a>
+            <section className="syncMenu-section wrap">
+              <Link to="/" className="syncMenu-item">
+                { activeAccount && <div>{addr}</div> }
+                <div>{ activeAccount ? (
+                  <button onClick={disconnect}>Unsync</button>
+                 ) : (
+                   <button onClick={connect}>Sync</button>
+                 )}</div>
+              </Link>
             </section>
           </section>
         </div>
