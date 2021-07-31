0ebab6f4ea6db08305eaceee785d0f2a11035958
kylegrover <kyle@kylegrover.com>
2021-07-30 20:17:55 -0700

    fetch polls

--- poll-taquito/src/pages/Polls.tsx
+++ poll-taquito/src/pages/Polls.tsx
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import '../assets/styles/utility-classes.css';
 import './polls.css';
 
@@ -5,42 +6,64 @@ import { ProposalCard } from '../components/ProposalCard';
 import { Select } from '../components/Select';
 import { Pagination } from '../components/Pagination';
 
+
 async function fetchPolls() {
   return await fetch(`https://api.florencenet.tzkt.io/v1/bigmaps/102344/keys`)
     .then(response => response.json())
     .then(polls => polls);
 }
 
-export const Polls = (props: any) => {
-  const viewTitles: any = {
+class Polls extends React.Component<{ view: string }, { polls: any[] }> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      polls: []
+    }
+
+    // test wallet: tz1LovFrdT3D2uY5ijGnb16172szCqFazDCE
+    fetchPolls().then(result => {
+      console.log(result)
+      this.setState({
+        polls: result
+      })
+    })
+  }
+  
+  viewTitles: any = {
     'proposals': 'Proposals',
     'questions': 'Questions',
     'my': 'My Votes',
     'past': 'Past Votes'
+  };
+
+  render() {
+
+    return (
+      <article className="polls">
+        <header className="pageHeader">
+          <h1>{ this.viewTitles[this.props.view] ?? 'All Votes' }</h1>
+        </header>
+        <nav className="pageSection polls-filters">
+          <Select />
+          <Select />
+        </nav>
+        <section className="pageSection polls-cards">
+          { this.state.polls.map((poll: any) => <ProposalCard poll={poll} />) }
+          {/* <ProposalCard />
+          <ProposalCard />
+          <ProposalCard />
+          <ProposalCard />
+          <ProposalCard />
+          <ProposalCard />
+          <ProposalCard />
+          <ProposalCard /> */}
+        </section>
+        <footer className="pageSection polls-pagination">
+          {/* <Pagination /> */}
+        </footer>
+      </article>
+    );
   }
-  return (
-    <article className="polls">
-      <header className="pageHeader">
-        <h1>{ viewTitles[props.view] ?? 'All Votes' }</h1>
-      </header>
-      <nav className="pageSection polls-filters">
-        <Select />
-        <Select />
-      </nav>
-      <section className="pageSection polls-cards">
-        { props.polls && props.polls.map((poll: any) => <ProposalCard poll={poll} />) }
-        {/* <ProposalCard />
-        <ProposalCard />
-        <ProposalCard />
-        <ProposalCard />
-        <ProposalCard />
-        <ProposalCard />
-        <ProposalCard />
-        <ProposalCard /> */}
-      </section>
-      <footer className="pageSection polls-pagination">
-        {/* <Pagination /> */}
-      </footer>
-    </article>
-  );
 }
+
+export {Polls};
