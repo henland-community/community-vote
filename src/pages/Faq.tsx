bcffe32fd44bc07c04dfbc9354034770e4e0bcdf
merchantcoppola <97635650+merchantcoppola@users.noreply.github.com>
2022-10-17 20:12:32 +0200

    Updted FAQ info
    missing: update for new unser snapshot script
    old:
    https://github.com/jagracar/hen-utils/blob/main/python/henUtils/henUsersSnapshot.py

--- src/pages/Faq.tsx
+++ src/pages/Faq.tsx
@@ -9,22 +9,23 @@ export const Faq = () => {
       </header>
       <section className="pageSection">
         <h2>Who can vote?</h2>
-        <p>Everyone who has used hicetnunc before the snapshot date (January 16, 2022) will have one vote in the initial rounds of h=n community voting. The community can audit <a href="https://vote.hencommunity.quest/hen-users-snapshot-16-01-2022.json" target="_blank" rel="noreferrer">the voter list</a>. Thanks to <a target="_blank" rel="noreferrer" href="https://twitter.com/jagracar">jagracar</a> for compiling this list, you can also audit <a target="_blank" rel="noreferrer" href="https://github.com/jagracar/hen-utils/blob/main/python/henUtils/henUsersSnapshot.py">his python script</a>.</p>
+        <p> The list of addresses of which cast votes will be counted includes all wallets that interacted with the teia or hicetnunc marketplace contracts or held hDAO before the last snapshot date (October 13th, 2022). Every eligible address has one vote. The community can audit <a href="https://raw.githubusercontent.com/teia-community/community-vote/master/public/teia_and_hen_users_snapshot_13-10-2022.json" target="_blank" rel="noreferrer">the voter list</a>. Thanks to <a target="_blank" rel="noreferrer" href="https://twitter.com/jagracar">jagracar</a> for compiling this list, you can also audit <a target="_blank" rel="noreferrer" href="https://github.com/jagracar/hen-utils/blob/main/python/henUtils/henUsersSnapshot.py">his python script</a>.</p>
       </section>
       <section className="pageSection">
         <h2>How are people voting?</h2>
-        <p>People may vote for any available option on any open proposal until the end of its voting period, and change their vote as many times as they like during the voting period (before it ends). Voting in version 1, will not include a token, or a representation of a token but a vote will simply be added to the tally of the voting results.</p>
+        <p>People may vote for any available option on any open proposal until the end of its voting period, and change their vote as many times as they like during the voting period (before it ends). 
+		Voting with this current setup will not include a token, or a representation of a token but a vote will simply be added to the tally of the voting results. DAO Token votes are planned for the upcoming <a href="https://github.com/teia-community/teia-docs/wiki/Governance-on-Teia" target="_blank">Teia DAO setup (under construction)</a></p>
         <p>This function should only work during a voting period.</p>
         <p>Gas fees for voting will be paid by voters. Current cost for an initial vote is ~0.017 tez (about 5 cents USD at the time of writing), the cost to change your vote is 0.0007 tez.</p>
         <p>Any voting cost is intrinsic to the blockchain, we charge no fee for voting. We aim to reduce gas prices even further in our next contract.</p>
       </section>
       <section className="pageSection">
         <h2>Can I delegate my vote?</h2>
-        <p>Not yet. This option is planned for further updates. Contribute to the planning, UI, and contract development work on <a href="https://github.com/hen-community/community-vote">GitHub</a>.</p>
+        <p>Not at the moment. Contribute to the planning, UI, and contract development of the voting tool on <a href="https://github.com/teia-community/community-vote/">GitHub</a>.</p>
       </section>
       <section className="pageSection">
         <h2>Where are the discussion and debates being held?</h2>
-        <p>We are using Discourse to give space for discussion and debate. You will be able to join the conversation there, or just read it, to make up your mind before voting. This tool is off-chain (hosted by <a href="https://twitter.com/_ufffd" target="_blank" rel="noreferrer">UFFFD</a>).</p>
+        <p>We are using <a href="https://discourse.teia.art/" target="_blank" rel="noreferrer">discourse.teia.art</a> to give space for discussion and debate of votes. You will be able to join the conversation there, or just read it, to make up your mind before voting. This tool is off-chain (hosted by <a href="https://twitter.com/_ufffd" target="_blank" rel="noreferrer">UFFFD</a>).</p>
         <p>On this discussion tool, you will be able to comment on a proposal, like a comment or a reply, share links and ideas.</p>
       </section>
       <section className="pageSection">
@@ -33,18 +34,12 @@ export const Faq = () => {
         These results can be independently verified by the voters using the unchangeable blockchain data.</p>
       </section>
       <section className="pageSection">
-        <h2>Who is behind this platform now?</h2>
-        <p>Right after the discontinuation of hicetnunc.xyz on November 11th, 2021, a group of people (initially mainly participants of the first “hicathon” event) joined forces to organize the future of the platform. The main discussion platform is the new “hen_community” (HENC) discord server and the community forum.</p>
-        <p>We are currently working to form a DAO in order to make this platform fully decentralized. Our main goals are to keep the core values and spirit of hicetnunc alive and to update the marketplace smart contract so that the fees can be used to keep the platform running.</p>
-        <p>The discord is always open for any kind of participation, and we will also continue to do our best to communicate and engage with the community on platforms other than discord.</p>
-      </section>
-      <section className="pageSection">
         <h2>How to participate?</h2>
         <ul>
-          <li>You can join the HENC discord here: Discord</li>
-          <li>You can also participate without joining the discord via the community forum: <a href="https://discourse.hencommunity.quest/" target="_blank" rel="noreferrer">discourse.hencommunity.quest</a></li>
-          <li>For updates, you can follow our current twitter account: <a href="https://twitter.com/hen_community" target="_blank" rel="noreferrer">@hen_community</a></li>
-          <li>Volunteer opportunities are available! If you are interested, please <a href="https://forms.gle/75QHgUC5EZTReceK7" target="_blank" rel="noreferrer">fill out this form</a> so we can get a overview and start coordinating the working groups (Note: This is not a prerequisite for participation; Anyone can join and participate, discuss, and look up past discussions.)</li>
+          <li>You can join the <a href="https://discord.gg/fmNTjfBdyV">Teia Discord Server</a></li> 
+          <li>You can also participate without joining the discord via the community forum <a href="https://discourse.teia.art/" target="_blank" rel="noreferrer">discourse.teia.art</a></li>
+          <li>For updates, you can follow our Blog <a href="https://blog.teia.art/" target="_blank" rel="noreferrer">blog.teia.art</a></li> and the official twitter account <a href="https://twitter.com/TeiaCommunity" target="_blank" rel="noreferrer">@TeiaCommunity</a></li>
+          <li>Anyone can join and participate, discuss, and look up past discussions.</li>
         </ul>
       </section>
     </article>
